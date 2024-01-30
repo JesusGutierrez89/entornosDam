e790d3e7f323bcb371396d9e451f79b851a9d2ee
JesusGutierrez89 <3687690@alu.murciaeduca.es>
2024-01-30 09:58:25 +0100

    prueba

---
+++ Ejemplo1.java
@@ -0,0 +1,11 @@
+public class Ejemplo1 {
+	public static void main (String []args){
+		System.out.println("Como estan ustedes");
+		String nombre="Jesus";
+		System.out.println("Mi nombre es "+nombre);
+	}
+}
+
+//1º paso para subir el archivo en git= git add
+//2º paso= git commer -am "Codigo comentario"
+//3º paso = git push origin NOMBRERAMA
